58919aa686eea25b86ff72880107c39600e427b3
sudhanshu09-cpu <srai79133@gmail.com>
2024-08-21 19:49:19 +0530

    Update RandomPass.py

--- RandomPass.py
+++ RandomPass.py
@@ -5,11 +5,9 @@ pass_len = 10
 charValues = string.ascii_letters + string.digits + string.punctuation
 
 # list comprehension is use for generating random password
-
 password= "".join([random.choice(charValues) for i in range(pass_len)])
 
 # using for loop to to generate random password
-
 # password = ""
 # for i in range(pass_len):
 #     password += (random.choice(charValues))
